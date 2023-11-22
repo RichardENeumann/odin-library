b9b3881bba5ef41d81cec9cd5d0149a02bb639a1
RichardENeumann <RichardNeumann@googlemail.com>
2023-11-22 11:55:59 +0100

    Implement drawLibrary function

--- main.js
+++ main.js
@@ -14,3 +14,28 @@ function Book(title, author, pages, haveRead) {
 function addBookToLibrary(title, author, pages, haveRead) {
     myLibrary[myLibrary.length] = new Book(title, author, pages, haveRead);
 }
+
+addBookToLibrary("Killing Floor", "Lee Child", 525, true);
+addBookToLibrary("A Game of Thrones", "George R.R. Martin", 807, true);
+addBookToLibrary("The Broken Sword", "Poul Anderson", 208, false);
+
+function drawLibrary() {
+    myLibrary.forEach(function (item) {
+        let tr = document.createElement("tr");
+        let title = document.createElement("td");
+            title.innerText = item.title;
+            tr.appendChild(title);
+        let author = document.createElement("td");
+            author.innerText = item.author;
+            tr.appendChild(author);
+        let pages = document.createElement("td");
+            pages.innerText = item.pages;
+            tr.appendChild(pages);
+        let haveRead = document.createElement("td");
+            haveRead.innerText = item.haveRead;
+            tr.appendChild(haveRead);
+        document.getElementById("libTable").appendChild(tr);
+    });
+}
+
+drawLibrary();
